3257565407307c752b08c5787c9df9ebd1f13cef
ofoster547 <ofoster@belcan.com>
2025-05-27 10:12:34 -0400

    Create UsageExamplefbs.cpp

---
+++ UsageExamplefbs.cpp
@@ -0,0 +1,43 @@
+#include "../proto/trade.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    // Create a new account with some orders
+    TradeProto::Account account(1, "Test", "USD", 1000);
+    account.Orders.emplace_back(TradeProto::Order(1, "EURUSD", TradeProto::OrderSide::BUY, TradeProto::OrderType::MARKET, 1.23456, 1000));
+    account.Orders.emplace_back(TradeProto::Order(2, "EURUSD", TradeProto::OrderSide::SELL, TradeProto::OrderType::LIMIT, 1.0, 100));
+    account.Orders.emplace_back(TradeProto::Order(3, "EURUSD", TradeProto::OrderSide::BUY, TradeProto::OrderType::STOP, 1.5, 10));
+
+    // Serialize the account to the FlatBuffer stream
+    flatbuffers::FlatBufferBuilder builder;
+    builder.Finish(account.Serialize(builder));
+
+    // Show original and FlatBuffer serialized sizes
+    std::cout << "Original size: " << account.size() << std::endl;
+    std::cout << "FlatBuffer size: " << builder.GetSize() << std::endl;
+
+    // Deserialize the account from the FlatBuffer stream
+    TradeProto::Account deserialized;
+    deserialized.Deserialize(*Trade::flatbuf::GetAccount(builder.GetBufferPointer()));
+
+    // Show account content
+    std::cout << std::endl;
+    std::cout << "Account.Id = " << deserialized.Id << std::endl;
+    std::cout << "Account.Name = " << deserialized.Name << std::endl;
+    std::cout << "Account.Wallet.Currency = " << deserialized.Wallet.Currency << std::endl;
+    std::cout << "Account.Wallet.Amount = " << deserialized.Wallet.Amount << std::endl;
+    for (auto& order : deserialized.Orders)
+    {
+        std::cout << "Account.Order => Id: " << order.Id
+            << ", Symbol: " << order.Symbol
+            << ", Side: " << (int)order.Side
+            << ", Type: " << (int)order.Type
+            << ", Price: " << order.Price
+            << ", Volume: " << order.Volume
+            << std::endl;
+    }
+
+    return 0;
+}
