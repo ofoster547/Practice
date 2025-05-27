65a60910e8a73cbacc7d12aa898e5fdc4546146d
ofoster547 <ofoster@belcan.com>
2025-05-27 11:00:12 -0400

    Create protobufUsageExample.cpp

---
+++ protobufUsageExample.cpp
@@ -0,0 +1,49 @@
+#include "../proto/trade.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    // Create a new account with some orders
+    TradeProto::Account account(1, "Test", "USD", 1000);
+    account.Orders.emplace_back(TradeProto::Order(1, "EURUSD", TradeProto::OrderSide::BUY, TradeProto::OrderType::MARKET, 1.23456, 1000));
+    account.Orders.emplace_back(TradeProto::Order(2, "EURUSD", TradeProto::OrderSide::SELL, TradeProto::OrderType::LIMIT, 1.0, 100));
+    account.Orders.emplace_back(TradeProto::Order(3, "EURUSD", TradeProto::OrderSide::BUY, TradeProto::OrderType::STOP, 1.5, 10));
+
+    // Serialize the account to the Protobuf stream
+    Trade::protobuf::Account output;
+    account.Serialize(output);
+    auto buffer = output.SerializeAsString();
+
+    // Show original and Protobuf serialized sizes
+    std::cout << "Original size: " << account.size() << std::endl;
+    std::cout << "Protobuf size: " << buffer.size() << std::endl;
+
+    // Deserialize the account from the Protobuf stream
+    Trade::protobuf::Account input;
+    input.ParseFromString(buffer);
+    TradeProto::Account deserialized;
+    deserialized.Deserialize(input);
+
+    // Show account content
+    std::cout << std::endl;
+    std::cout << "Account.Id = " << deserialized.Id << std::endl;
+    std::cout << "Account.Name = " << deserialized.Name << std::endl;
+    std::cout << "Account.Wallet.Currency = " << deserialized.Wallet.Currency << std::endl;
+    std::cout << "Account.Wallet.Amount = " << deserialized.Wallet.Amount << std::endl;
+    for (auto& order : deserialized.Orders)
+    {
+        std::cout << "Account.Order => Id: " << order.Id
+            << ", Symbol: " << order.Symbol
+            << ", Side: " << (int)order.Side
+            << ", Type: " << (int)order.Type
+            << ", Price: " << order.Price
+            << ", Volume: " << order.Volume
+            << std::endl;
+    }
+
+    // Delete all global objects allocated by Protobuf
+    google::protobuf::ShutdownProtobufLibrary();
+
+    return 0;
+}
